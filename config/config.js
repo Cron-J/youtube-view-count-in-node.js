888512ecc74598d131ca237fd7534af1a5a92ddb
gauravgupta90 <gaurav_bng@hotmail.com>
2015-01-26 04:15:05 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     db: "mysql://root@localhost/test",
     port: "3000",
-    cronTime: '00 26 01 * * *',
+    cronTime: '00 59 23 * * *',
     timeZone: "Asia/Kolkata",
     apiKey: 'AIzaSyChpdkNkPW-KuuKsPWpJaQtX20e--gvfM0'
 };
